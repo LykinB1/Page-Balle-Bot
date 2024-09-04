a7b8e77466fa8f6d93d0c1c2a4bd89d93e0a391e
G.O.S <arthurg.o.sb1@gmail.com>
2024-09-04 02:13:08 -0300

    Landing Page Complete

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+const navButton = document.querySelector(".nav-button");
+
+const nav = document.querySelector(".nav");
+
+navButton.addEventListener("click", () => nav.classList.toggle("active"));
+
